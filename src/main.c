e53be6aec2970413dac01a9d37462352a88506a0
Harold Freeman <haroldfreeman@protonmail.com>
2019-05-01 23:25:04 -0700

    Wip

--- src/main.c
+++ src/main.c
@@ -19,8 +19,9 @@
 #include <signal.h>
 #include <ncurses.h>
 #include <unistd.h>
-
-void* resizeHandler(int);
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
     // Setup ncurses
@@ -33,18 +34,77 @@ int main(int argc, char *argv[]) {
     curs_set(FALSE);
     // Capture special keys
     keypad(stdscr, TRUE);
+	// Seed random number generator
+	srand(time(NULL));
 
-    int running = 1;
+    int running = true;
+
+	// Build square list and colors
+	const char * square_names[] = {
+		"a1","a2","a3","a4","a5","a6","a7","a8",
+		"b1","b2","b3","b4","b5","b6","b7","b8",
+		"c1","c2","c3","c4","c5","c6","c7","c8",
+		"d1","d2","d3","d4","d5","d6","d7","d8",
+		"e1","e2","e3","e4","e5","e6","e7","e8",
+		"f1","f2","f3","f4","f5","f6","f7","f8",
+		"g1","g2","g3","g4","g5","g6","g7","g8",
+		"h1","h2","h3","h4","h5","h6","h7","h8"
+	};
+	const char square_colors[] = {
+		'b','w','b','w','b','w','b','w',
+		'w','b','w','b','w','b','w','b',
+		'b','w','b','w','b','w','b','w',
+		'w','b','w','b','w','b','w','b',
+		'b','w','b','w','b','w','b','w',
+		'w','b','w','b','w','b','w','b',
+		'b','w','b','w','b','w','b','w',
+		'w','b','w','b','w','b','w','b'
+	};
+
+	// Store the correct attempts
+	int correct = 0;
+	int total_attempts = 0;
 
     while(running) {
         clear();
+		// Select square
+		int r = rand() % 64;
+		move(0,0);
+		printw("Chess Vision Trainer");
+		move(1,0);
+		printw("--------------------");
+
+		move(2,0);
+		printw("Correct: %d", correct);
+		move(3,0);
+		printw("Total attempts: %d", total_attempts);
+		move(4,0);
+		printw("Percent correct: %.1f",
+				(correct / (double)total_attempts) * 100);
+
+		move(6,0);
+		printw("Is the square (b)lack or (w)hite:");
+		move(6, 34);
+		printw(square_names[r]);
+
+		// Get the selection
         int ch = getch();
         switch (ch) {
             case KEY_BACKSPACE: /* user pressed backspace */
                 running = 0;
                 break;
-            default:
-                mvaddch(1, 1, ch);
+			case 'w':
+				total_attempts++;
+				if (square_colors[r] == 'w') {
+					correct++;
+				}
+				break;
+			case 'b':
+				total_attempts++;
+				if (square_colors[r] == 'b') {
+					correct++;
+				}
+				break;
         }
         refresh();
     }
